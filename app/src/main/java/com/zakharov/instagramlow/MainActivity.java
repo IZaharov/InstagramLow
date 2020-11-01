da914aef796ea10a5f6d530b393b32263d15eaa9
Zakharov <i.zaharov98@mail.ru>
2020-11-01 22:00:39 +0300

    Добавлена панель приложения, на которой размещена кнопка для перехода в другое рпложение.

--- app/src/main/java/com/zakharov/instagramlow/MainActivity.java
+++ app/src/main/java/com/zakharov/instagramlow/MainActivity.java
@@ -2,17 +2,23 @@ package com.zakharov.instagramlow;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.ComponentName;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
-    public static ArrayList<String> list = new ArrayList();
+    public static ArrayList<String> list = new ArrayList<String>();
     private static ImageParser imageParser = new ImageParser();
     RecyclerView imagesRecyclerView;
     boolean isLoading = false;
@@ -21,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         imagesRecyclerView = findViewById(R.id.recyclerView);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
@@ -50,11 +58,10 @@ public class MainActivity extends AppCompatActivity {
         new DownloadImages().execute();
     }
 
-    public class DownloadImages extends AsyncTask<Void ,Void, Void>{
+    public class DownloadImages extends AsyncTask<Void ,Void, Void> {
         int index = 0;
         @Override
         protected void onPreExecute() {
-            super.onPreExecute();
             index = list.size();
             list.add(null);
             imagesRecyclerView.getAdapter().notifyItemInserted(index);
@@ -69,11 +76,42 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(Void aVoid) {
-            super.onPostExecute(aVoid);
             list.remove(index);
             imagesRecyclerView.getAdapter().notifyItemRemoved(index);
             imagesRecyclerView.getAdapter().notifyDataSetChanged();
             isLoading = false;
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_share_friendly_app:
+                //плохой вариант, зато открывается новое приложение
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_TEXT, "Hello from InstagramLow");
+                String chooserTitle = getString(R.string.chooser);
+                Intent chosenIntent = Intent.createChooser(intent, chooserTitle);
+                startActivity(chosenIntent);
+
+                //по идее нужный вариант, но открывается в текущем приложении
+//                Intent intent = new Intent("com.projectkfc.action.OPEN_FRIENDLY_APP");
+//                try{
+//                    startActivity(intent);
+//                }
+//                catch(Exception e){
+//                    e.printStackTrace();
+//                }
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 }
